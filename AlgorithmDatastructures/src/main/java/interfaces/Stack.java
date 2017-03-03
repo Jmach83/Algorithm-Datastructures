fe043dc1cbdbc630735d01a07f7e45a5efca2d3a
Jonas Machon <Jonas Machon>
2017-03-03 10:14:41 +0100

    Airport template

---
+++ AlgorithmDatastructures/src/main/java/interfaces/Stack.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author Jmach
+ */
+public interface Stack<T>
+{
+    void push(T item);
+    T pop();
+    int size();
+    default boolean isEmpty() {return size() == 0; }        
+}
