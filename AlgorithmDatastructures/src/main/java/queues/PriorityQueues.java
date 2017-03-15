54e206d4b1147055b2d88768d3730d3553c943ea
Jonas Machon <Jonas Machon>
2017-03-15 13:56:38 +0100

    Graph interface

--- AlgorithmDatastructures/src/main/java/queues/PriorityQueues.java
+++ AlgorithmDatastructures/src/main/java/queues/PriorityQueues.java
@@ -1,41 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package queues;
-import interfaces.PriorityQueue;
-import java.util.NoSuchElementException;
-
-/**
- *
- * @author Jmach
- */
-public class PriorityQueues<T> implements PriorityQueue<Comparable<T>>
-{
-
-    @Override
-    public void enqueue(Comparable<T> item)
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Comparable<T> dequeue() throws NoSuchElementException
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Comparable<T> peek() throws NoSuchElementException
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int size()
-    {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package queues;
+//import interfaces.PriorityQueue;
+//import java.util.NoSuchElementException;
+//
+///**
+// *
+// * @author Jmach
+// */
+//public class PriorityQueues<T> implements PriorityQueue<Comparable<T>>
+//{
+//
+//    @Override
+//    public void enqueue(Comparable<T> item)
+//    {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public Comparable<T> dequeue() throws NoSuchElementException
+//    {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public Comparable<T> peek() throws NoSuchElementException
+//    {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public int size()
+//    {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//    
+//}
